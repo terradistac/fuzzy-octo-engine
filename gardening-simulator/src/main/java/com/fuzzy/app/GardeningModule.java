67553117ff25d778d6a24782142582bfe5edf950
Christina Terradista <terradistacc@gmail.com>
2020-06-30 19:21:29 -0400

    Some restructuring.

---
+++ gardening-simulator/src/main/java/com/fuzzy/app/GardeningModule.java
@@ -0,0 +1,14 @@
+package com.fuzzy.app;
+import com.fuzzy.module.Module;
+
+/**
+ * Hello world!
+ *
+ */
+public class GardeningModule implements Module 
+{
+    @Override
+    public void execute() {
+
+    }
+}
